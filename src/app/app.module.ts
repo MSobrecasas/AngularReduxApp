a8a4ccae705985f19ecbec77f98abbef9c48d049
MSobrecasas <sns.mary@hotmail.com>
2023-12-07 17:43:13 -0300

    seccion 8 finalizada y funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from 'src/environments/environment';
 
 import { StoreModule } from '@ngrx/store';
-// import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { appReducers } from './app.reducer';
 
 @NgModule({
@@ -46,7 +46,11 @@ import { appReducers } from './app.reducer';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
-    StoreModule.forRoot(appReducers)
+    StoreModule.forRoot(appReducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,39 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
+
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import * as ui from '../../shared/ui.actions';
+
 import Swal from 'sweetalert2';
+import { AuthService } from '../../services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styles: []
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
 
   registroForm: FormGroup;
+  cargando: boolean = false;
+  uiSubscription: Subscription;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private store: Store<AppState>,
+    private router: Router) { }
+
+  ngOnInit() {
+
     this.registroForm = this.fb.group({
       nombre: ['', Validators.required],
       correo: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
+
+    this.uiSubscription = this.store.select('ui')
+      .subscribe(ui => this.cargando = ui.isLoading);
   }
 
-  ngOnInit() {
+  ngOnDestroy() {
+    this.uiSubscription.unsubscribe();
   }
 
   crearUsuario() {
 
     if (this.registroForm.invalid) { return; }
 
-    Swal.fire({
-      title: 'Espere por favor',
-      onBeforeOpen: () => {
-        Swal.showLoading()
-      }
-    });
+    // Swal.fire({
+    //   title: 'Espere por favor',
+    //   onBeforeOpen: () => {
+    //     Swal.showLoading()
+    //   }
+    // });
+
+    this.store.dispatch(ui.isLoading());
 
 
     const { nombre, correo, password } = this.registroForm.value;
@@ -42,11 +62,13 @@ export class RegisterComponent implements OnInit {
       .then(credenciales => {
         console.log(credenciales);
 
-        Swal.close();
+        // Swal.close();
+        this.store.dispatch(ui.stopLoading());
 
         this.router.navigate(['/']);
       })
       .catch(err => {
+        this.store.dispatch(ui.stopLoading());
         Swal.fire({
           icon: 'error',
           title: 'Oops...',
@@ -54,4 +76,5 @@ export class RegisterComponent implements OnInit {
         })
       });
   }
+
 }
